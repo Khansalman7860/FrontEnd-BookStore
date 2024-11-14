5ba615e4585c996af149ff9e3b4d4f6ab828ab12
Khansalman7860 <Baadshah@eng.rizvi.edu.in>
2024-11-14 11:13:00 +0000

    front version 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/navbar.jsx";
+import Banner from "./components/banner.jsx";
+import Footer from "./components/footer.jsx";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
